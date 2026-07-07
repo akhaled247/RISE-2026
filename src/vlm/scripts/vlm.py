b7b333553ed4b058359c49b52ec16ad88ca830ed
James Li <jamespeilunli@gmail.com>
2026-07-07 14:19:34 +0000

    proof of concept vision+language inference

--- src/vlm/scripts/vlm.py
+++ src/vlm/scripts/vlm.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
+import sys
+import rospkg
 import numpy as np
 import rospy
+import base64
+import os
 from openai import OpenAI
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
 
 def main(): 
     client = OpenAI(
@@ -10,18 +18,35 @@ def main():
         base_url="http://localhost:49173/v1"
     )
 
-    response = client.responses.create(
-        model="Qwen/Qwen3.5-4B",
-        reasoning={"effort": "none"},
-        input=[{
-            "role": "user",
-            "content": [
-                {"type": "input_text", "text": "Who are you?"},
-            ],
-        }],
-    )
+    rospack = rospkg.RosPack()
+    image_path = os.path.join(rospack.get_path('vlm'), 'assets', 'leclerc.jpg')
+    base64_image = encode_image(image_path)
 
-    print("RESPONSE:", response.output_text)
+    try:
+        response = client.chat.completions.create(
+            model="Qwen/Qwen3.5-4B",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": "Who is in this image?"},
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}"
+                            },
+                        },
+                    ],
+                }
+            ],
+        )
+    except Exception as e:
+        print("Error", e)
+        sys.exit(1)
+
+    print("RESPONSE:")
+    print(response.choices[0].message.content)
+    sys.exit(0)
 
 if __name__ == "__main__":
     rospy.init_node("vlm")
