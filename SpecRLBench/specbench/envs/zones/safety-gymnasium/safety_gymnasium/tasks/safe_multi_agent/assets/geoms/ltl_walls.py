071526 rotation fixes

SpecRLBench/specbench/envs/zones/safety-gymnasium/safety_gymnasium/tasks/safe_multi_agent/assets/geoms/ltl_walls.py
@@ -86,9 +86,17 @@ class LtlWalls(Geom):  # pylint: disable=too-many-instance-attributes
         self.index += 1
         self.index %= self.num
 
+    def process_config(self, config, layout, rots):
+        self.index = 0
+        return super().process_config(config, layout, rots)
+
     def get_config(self, xy_pos, rot):  # pylint: disable=unused-argument
         """To facilitate get specific config for this object."""
-        rot = [np.arctan2(y - self.d_y, x - self.d_x) for x, y in self.locations][self.index]
+        xy_pos = np.asarray(xy_pos, dtype=float)
+        if self.name.startswith('building') and self.name.endswith('_ltl_walls'):
+            rot = float(np.arctan2(xy_pos[1] - self.d_y, xy_pos[0] - self.d_x))
+        else:
+            rot = [np.arctan2(y - self.d_y, x - self.d_x) for x, y in self.locations][self.index]
         body = {
             'name': self.name,
             'pos': np.r_[xy_pos, self.height],

SpecRLBench/specbench/envs/zones/safety-gymnasium/safety_gymnasium/tasks/safe_multi_agent/tasks/multi_goal_sar/multi_sar_level0.py
@@ -209,8 +209,8 @@ class MultiGoalSARLevel0(BaseTask):
             wall = getattr(self, name)
             center_xy = layout[f'{building_prefix}{wall_idx}']
             rot = self._cached_building_rots[wall_idx]
-            wall.rots = [rot] * wall.num
             self._sync_building_ltl_wall_site(wall, center_xy, rot)
+            wall.index = 0
             for seg_idx, loc in enumerate(wall.locations):
                 layout[f'building{wall_idx}_ltl_wall{seg_idx}'] = np.asarray(loc, dtype=float)
 