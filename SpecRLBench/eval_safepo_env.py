b27fa53d2157dc8209e1ded9df500f4ce6dfaec1
akhaled247 <abdullahkhaled0224@gmail.com>
2026-07-22 17:15:44 -0400

    072226 potential models

--- SpecRLBench/eval_safepo_env.py
+++ SpecRLBench/eval_safepo_env.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 """
     python ~/RISE-2026/SpecRLBench/eval_safepo_env.py --run-dir \
-    ~/RISE-2026/SpecRLBench/_training_logs/safepo/PointLTL5MASAR1WC-v0/ppo/seed-000-2026-07-22-11-47-36 \
+    ~/RISE-2026/SpecRLBench/_training_logs/safepo/PointLTL5MASAR1WC-v0/trpo/seed-000-2026-07-22-13-54-45 \
     --eval-episodes 50
 """
 
