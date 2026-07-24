8db8079c263be8aa633fc0d92b4e9d1294b7a683
akhaled247 <abdullahkhaled0224@gmail.com>
2026-07-24 09:39:01 -0400

    072426 progress viz

--- SpecRLBench/eval_safepo_env.py
+++ SpecRLBench/eval_safepo_env.py
@@ -16,10 +16,9 @@ from pathlib import Path
 
 """
     python ~/RISE-2026/SpecRLBench/eval_safepo_env.py --run-dir \
-    ~/RISE-2026/SpecRLBench/_training_logs/safepo/PointLTL5MASAR1WC-v0/trpo/seed-000-2026-07-22-13-54-45 \
+    ~/RISE-2026/SpecRLBench/_training_logs/safepo/PointLTL5MASAR1WC-v0/trpo/seed-000-2026-07-24-07-44-15 \
     --eval-episodes 50
 """
-
 
 ROOT = Path(__file__).resolve().parent
 sys.path.insert(0, str(ROOT))
