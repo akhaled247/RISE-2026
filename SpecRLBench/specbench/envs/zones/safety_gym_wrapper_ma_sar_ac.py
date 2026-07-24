001ebc9f27497d951b509b16082dc63f188ba263
akhaled247 <abdullahkhaled0224@gmail.com>
2026-07-24 15:10:34 -0400

    072426 wrapper added forgot before

---
+++ SpecRLBench/specbench/envs/zones/safety_gym_wrapper_ma_sar_ac.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+import gymnasium
+import numpy as np
+from gymnasium import spaces
+from gymnasium.core import ActType, WrapperObsType
+from gymnasium.spaces import Box
+from specbench.envs.zones.safety_gym_wrapper_ma_sar_wc import SafetyGymWrapperMASARWC
+from specbench.utils.ltl.logic import Assignment
+from safety_gymnasium.tasks.safe_multi_agent.utils.sar_utils import (
+    agent_has_entrapped_at_building,
+    agent_inside_building_idx,
+)
+
+class SafetyGymWrapperMASARAC(SafetyGymWrapperMASARWC):
+    def step(self, action):
+        obs, reward, terminated, truncated, info = super().step(action)
+        info["cost"] = 0
+        for a in self.env.unwrapped.possible_agents:
+            try:
+                if info[a].get("cost_collision") > 0:
+                    info["cost"] += 1
+                    terminated[a] = True
+            except Exception as e:
+                pass
+        return obs, reward, terminated, truncated, info
+
+    def reset(self, *, seed=None, options=None):
+        obs, info = super().reset(seed=seed, options=options)
+        info["cost"] = 0
+        return obs, info
