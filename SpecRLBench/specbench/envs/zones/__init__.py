f172793204f151cac876ba7b4ba66120079bf7e8
akhaled247 <abdullahkhaled0224@gmail.com>
2026-07-28 08:04:05 -0400

    Remove vendored SpecRLBench before submodule

--- SpecRLBench/specbench/envs/zones/__init__.py
+++
@@ -1,9 +0,0 @@
-from .wrappers import (
-    SafetyGymWrapper,
-    SafetyGymWrapperMA,
-    SafetyGymWrapperMASAR,
-    SafetyGymWrapperMASARWC,
-    SafetyGymWrapperMASARAC,
-)
-from .make_env import make_zone_env
-from .safety_gym_register import register_helper
--- SpecRLBench/specbench/envs/zones/safety_gym_wrapper.py
+++
@@ -1,5 +0,0 @@
-"""Upstream-compatible re-export (wrappers live in ``wrappers/``)."""
-
-from specbench.envs.zones.wrappers.safety_gym_wrapper import SafetyGymWrapper
-
-__all__ = ["SafetyGymWrapper"]
